e7dac8d56baf5028198c357107dfe11908e0cf75
Assaf Arkin <assaf@labnotes.org>
2021-11-09 09:41:19 -0800

    Fix analytics code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -36,9 +36,11 @@ export default class MyDocument extends Document {
                 async
                 src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
               />
-              <script>
-                {`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');`}
-              </script>
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');`,
+                }}
+              ></script>
             </>
           )}
         </body>
