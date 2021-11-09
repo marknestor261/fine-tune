39636059924011080aaca2685ad3cd9203d74de3
Assaf Arkin <assaf@labnotes.org>
2021-11-09 09:21:20 -0800

    Added GA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -30,6 +30,17 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          {process.env.GOOGLE_ANALYTICS_ID && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+              />
+              <script>
+                {`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');`}
+              </script>
+            </>
+          )}
         </body>
       </Html>
     );
